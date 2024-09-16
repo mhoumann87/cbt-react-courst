2b994ff63fce3de3c3e52e67ce756033a9fb2558
Michael Houmann <mhoumann87@gmail.com>
2024-09-16 22:15:48 +0200

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-//import './App.css';
+import Employee from './components/Employee';
+import './App.css';
 
 function App() {
+  const showEmployees = false;
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>Hello</p>
+        {showEmployees ? <Employee /> : <p>You can't see the employees!</p>}
       </header>
     </div>
   );
