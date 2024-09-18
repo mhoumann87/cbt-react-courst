461b60316ead9745a5da26734d58f2c4178f99e2
Michael Houmann <mhoumann87@gmail.com>
2024-09-18 15:42:20 +0200

    Add modal ond a styled form. Add react-bootstrap

---
+++ src/components/EditEmployee.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+function EditEmployee() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button
+        onClick={handleShow}
+        className='px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:ring-offset-2'>
+        Update
+      </button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop='static'
+        keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <form id='editModal' className='w-full max-w-sm'>
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  htmlFor='name'
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'>
+                  Name
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded-w-full py-2 px-4 text-gray-700 leading-tight focus-outline-none focus:bg-white focus:border-purple-500'
+                  id='name'
+                  type='text'
+                  value='Jane Doe'
+                />
+              </div>
+            </div>
+
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  htmlFor='role'
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'>
+                  Role
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded-w-full py-2 px-4 text-gray-700 leading-tight focus-outline-none focus:bg-white focus:border-purple-500'
+                  id='role'
+                  type='text'
+                  value='Manager'
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button
+            onClick={handleClose}
+            className='bg-slate-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded'>
+            Close
+          </button>
+
+          <button
+            form='editModal'
+            className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded'>
+            Update
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default EditEmployee;
