7d7303d03d8789608b054f06ea2322317b40a094
Michael Houmann <mhoumann87@gmail.com>
2024-09-23 14:01:07 +0200

    Add react router and refactor nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
+import Employees from './pages/Employees';
+import Clients from './pages/Clients';
 
 function App() {
   return (
-    <Header>
-      <Employees />;
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path='/employees' element={<Employees />} />
+          <Route path='/clients' element={<Clients />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Clients.js
@@ -0,0 +1,9 @@
+function Clients() {
+  return (
+    <div className='bg-slate-600 min-h-screen'>
+      <h1>Clients</h1>
+    </div>
+  );
+}
+
+export default Clients;
